9ea5c91f5c6d34b0a056bfafd9867730ae65d316
Mike <kordiak@gmail.com>
2018-03-07 16:37:30 -0500

    First Tests

--- script.js
+++ script.js
@@ -1,12 +1,67 @@
-let d = Date.now();
+function runner(){
 
-let myDate = new Date();
+    //need to define a time, say 5 minutes
+    //then need to set current time
+    //set future time
+    //calc now - then to get remaining time.
 
-let e = myDate.getTime();
+    //milliseconds to seconds .001/1
+    let milli = 1000;
+    let seconds = 60;
+    let minutes = .25;
 
-console.log(d);
-console.log(e);
+    let milliTimer = milli * seconds * minutes;
+
+    
+    let timeNow = new Date(Date.now());
+
+    let timerAlarm = new Date(timeNow.getTime() + milliTimer);
+
+    let timer = setInterval(()=>{
+        let timeNow = new Date(Date.now());
+
+        let timeLeft = new Date(timerAlarm.getTime() - timeNow);
+        
+        if(timeLeft <=10){
+            clearInterval(timer);
+            console.log('done');
+        }
+
+        console.log(timeLeft.getMinutes(), ':', timeLeft.getSeconds());
+        $('#timer').text(timeLeft);
+    
+    }, 1000);
+}
+
+$("document").ready(function(){
 
 
 
-console.log('Running!');
+    //runner();
+
+}); 
+
+
+
+function controlButton(defaultValue){
+
+    let val = defaultValue;
+
+    this.increment = ()=>{
+        val++;
+    }
+
+    this.decrement = ()=>{
+        val--;
+    }
+
+    this.value = ()=>{
+        return val;
+    }
+
+}
+
+let minSetter = new controlButton(5);
+minSetter.increment();
+minSetter.increment();
+console.log(minSetter.value());
